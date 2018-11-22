cbf42e7e4e2e44dd6b7dd51c9bbb922967564dd7
Gear61 <chessnone@yahoo.com>
2018-11-22 15:46:52 -0800

    Merge term and definition entry for flashcard creation

--- app/src/main/java/com/randomappsinc/simpleflashcards/dialogs/CreateFlashcardDialog.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/dialogs/CreateFlashcardDialog.java
@@ -2,10 +2,14 @@ package com.randomappsinc.simpleflashcards.dialogs;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.widget.EditText;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.randomappsinc.simpleflashcards.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CreateFlashcardDialog {
 
@@ -13,70 +17,32 @@ public class CreateFlashcardDialog {
         void onFlashcardCreated(String term, String definition);
     }
 
-    protected MaterialDialog termDialog;
-    protected MaterialDialog definitionDialog;
+    @BindView(R.id.term_input) EditText termInput;
+    @BindView(R.id.definition_input) EditText definitionInput;
+
+    protected MaterialDialog dialog;
 
     public CreateFlashcardDialog(Context context, @NonNull final Listener listener) {
-        termDialog = new MaterialDialog.Builder(context)
-                .title(R.string.set_flashcard_term)
-                .alwaysCallInputCallback()
-                .input(context.getString(R.string.term),
-                        "",
-                        new MaterialDialog.InputCallback() {
-                            @Override
-                            public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
-                                String term = input.toString();
-                                boolean notEmpty = !term.trim().isEmpty();
-                                dialog.getActionButton(DialogAction.POSITIVE).setEnabled(notEmpty);
-                            }
-                        })
-                .positiveText(R.string.next)
-                .negativeText(R.string.cancel)
+        dialog = new MaterialDialog.Builder(context)
+                .title(R.string.create_flashcard)
+                .customView(R.layout.create_flashcard, true)
+                .positiveText(R.string.save)
                 .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                        definitionDialog.show();
-                    }
-                })
-                .build();
-        termDialog.getInputEditText().setSingleLine(false);
-        definitionDialog = new MaterialDialog.Builder(context)
-                .title(R.string.set_flashcard_definition)
-                .alwaysCallInputCallback()
-                .input(context.getString(R.string.definition),
-                        "",
-                        new MaterialDialog.InputCallback() {
-                            @Override
-                            public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
-                                String definition = input.toString();
-                                boolean notEmpty = !definition.trim().isEmpty();
-                                dialog.getActionButton(DialogAction.POSITIVE).setEnabled(notEmpty);
-                            }
-                        })
-                .positiveText(R.string.create)
-                .negativeText(R.string.cancel)
-                .neutralText(R.string.back)
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                        String term = termDialog.getInputEditText().getText().toString().trim();
-                        String definition = dialog.getInputEditText().getText().toString().trim();
+                        String term = termInput.getText().toString().trim();
+                        String definition = definitionInput.getText().toString().trim();
                         listener.onFlashcardCreated(term, definition);
                     }
                 })
-                .onNeutral(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
-                        termDialog.show();
-                    }
-                })
+                .negativeText(R.string.cancel)
                 .build();
-        definitionDialog.getInputEditText().setSingleLine(false);
+        ButterKnife.bind(this, dialog.getCustomView());
     }
 
     public void show() {
-        termDialog.getInputEditText().setText("");
-        definitionDialog.getInputEditText().setText("");
-        termDialog.show();
+        termInput.setText("");
+        definitionInput.setText("");
+        dialog.show();
     }
 }
