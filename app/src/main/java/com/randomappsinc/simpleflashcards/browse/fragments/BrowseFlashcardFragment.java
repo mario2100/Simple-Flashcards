f5a6495896c20cfbb4428ea53d49bd850f9393eb
Gear61 <chessnone@yahoo.com>
2019-03-06 23:55:33 -0800

    Make text scrollable in browse flashcards

--- app/src/main/java/com/randomappsinc/simpleflashcards/browse/fragments/BrowseFlashcardFragment.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/browse/fragments/BrowseFlashcardFragment.java
@@ -3,17 +3,13 @@ package com.randomappsinc.simpleflashcards.browse.fragments;
 import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
 import android.text.TextUtils;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +19,6 @@ import android.widget.TextView;
 
 import com.randomappsinc.simpleflashcards.R;
 import com.randomappsinc.simpleflashcards.browse.activities.BrowseFlashcardsActivity;
-import com.randomappsinc.simpleflashcards.browse.dialogs.FullFlashcardTextDialog;
 import com.randomappsinc.simpleflashcards.browse.managers.BrowseFlashcardsSettingsManager;
 import com.randomappsinc.simpleflashcards.common.Constants;
 import com.randomappsinc.simpleflashcards.common.activities.PictureFullViewActivity;
@@ -57,17 +52,17 @@ public class BrowseFlashcardFragment extends Fragment {
     @BindView(R.id.position_info) TextView positionInfo;
     @BindView(R.id.side_header) TextView sideHeader;
     @BindView(R.id.speak) View speak;
-    @BindView(R.id.card_image) ImageView cardImage;
-    @BindView(R.id.content) TextView content;
     @BindView(R.id.flip_icon) View flipIcon;
-    @BindView(R.id.content_container) View contentContainer;
+    @BindView(R.id.content_container) ViewGroup contentContainer;
+
+    protected ImageView cardImage;
+    protected TextView content;
 
     @BindInt(R.integer.default_anim_length) int flipAnimLength;
     @BindString(R.string.view_all) String viewAll;
 
     protected Flashcard flashcard;
     protected boolean isShowingTerm;
-    private FullFlashcardTextDialog fullTextDialog;
     private BrowseFlashcardsSettingsManager settingsManager = BrowseFlashcardsSettingsManager.get();
     private Unbinder unbinder;
 
@@ -95,12 +90,6 @@ public class BrowseFlashcardFragment extends Fragment {
         loadFlashcardIntoView();
 
         return rootView;
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        fullTextDialog = new FullFlashcardTextDialog(getActivity());
     }
 
     @OnClick(R.id.flashcard_container)
@@ -149,17 +138,39 @@ public class BrowseFlashcardFragment extends Fragment {
     protected void loadFlashcardIntoView() {
         sideHeader.setText(isShowingTerm ? R.string.term_underlined : R.string.definition_underlined);
 
+        // Load proper layout for card content based on orientation
+        contentContainer.removeAllViews();
+        int orientation = getActivity().getResources().getConfiguration().orientation;
+        View contentView;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            contentView = LayoutInflater
+                    .from(getContext())
+                    .inflate(R.layout.browse_flashcard_content_horizontal, contentContainer);
+        } else {
+            contentView = LayoutInflater
+                    .from(getContext())
+                    .inflate(R.layout.browse_flashcard_content_vertical, contentContainer);
+        }
+        content = contentView.findViewById(R.id.content);
+        content.setMovementMethod(new ScrollingMovementMethod());
+        cardImage = contentView.findViewById(R.id.card_image);
+        cardImage.setOnClickListener(imageClickListener);
+
         String contentText = isShowingTerm ? flashcard.getTerm() : flashcard.getDefinition();
         if (TextUtils.isEmpty(contentText)) {
             content.setVisibility(View.GONE);
         } else {
             content.setText(contentText);
-            content.setVisibility(View.INVISIBLE);
+            content.setVisibility(View.VISIBLE);
         }
 
+        setUpImageView();
+    }
+
+    private void setUpImageView() {
         final String imageUrl = isShowingTerm ? flashcard.getTermImageUrl() : flashcard.getDefinitionImageUrl();
         if (!TextUtils.isEmpty(imageUrl)) {
-            cardImage.setVisibility(View.INVISIBLE);
+            cardImage.setVisibility(View.VISIBLE);
             if (ViewCompat.isLaidOut(cardImage)) {
                 loadImage(imageUrl);
             } else {
@@ -173,75 +184,6 @@ public class BrowseFlashcardFragment extends Fragment {
         } else {
             cardImage.setVisibility(View.GONE);
         }
-        maybeAdjustContent();
-    }
-
-    /**
-     * It's possible that the user put in too much text for the flashcard container.
-     * If so, we need to truncate it.
-     */
-    private void maybeAdjustContent() {
-        contentContainer.post(new Runnable() {
-            @Override
-            public void run() {
-                int containerHeight = contentContainer.getHeight()
-                        - getResources().getDimensionPixelSize(R.dimen.browse_card_content_padding) * 2;
-                int cardImageHeight = cardImage.getVisibility() == View.GONE ? 0 : cardImage.getHeight();
-                int textHeight = content.getVisibility() == View.GONE ? 0 : content.getHeight();
-                if (containerHeight < cardImageHeight + textHeight) {
-                    adjustContentText(containerHeight - cardImageHeight);
-                }
-                content.setVisibility(View.VISIBLE);
-                if (cardImage.getVisibility() == View.INVISIBLE) {
-                    cardImage.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-    }
-
-    protected void adjustContentText(int allowedHeight) {
-        final String fullText = content.getText().toString();
-        int start = 1;
-        int end = fullText.length();
-        while (start < end) {
-            int mid = start + (end-start) / 2;
-            int height = ViewUtils.measureHeightOfText(
-                    fullText.substring(0, mid),
-                    (int) content.getTextSize(),
-                    content.getWidth());
-            if (height <= allowedHeight) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-
-        String truncationText = "... " + viewAll;
-        String finalText = fullText.substring(0, start - 1 - truncationText.length()) + truncationText;
-        SpannableString textWithViewAll = new SpannableString(finalText);
-        ClickableSpan clickableSpan = new ClickableSpan() {
-            @Override
-            public void onClick(@NonNull View textView) {
-                showFullTextInDialog(fullText);
-            }
-
-            @Override
-            public void updateDrawState(@NonNull TextPaint textPaint) {
-                super.updateDrawState(textPaint);
-            }
-        };
-        textWithViewAll.setSpan(
-                clickableSpan,
-                finalText.length() - viewAll.length(),
-                finalText.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        content.setText(textWithViewAll);
-        content.setMovementMethod(LinkMovementMethod.getInstance());
-        content.setHighlightColor(Color.BLUE);
-    }
-
-    protected void showFullTextInDialog(String fullText) {
-        fullTextDialog.show(fullText, isShowingTerm);
     }
 
     protected void loadImage(String imageUrl) {
@@ -282,27 +224,34 @@ public class BrowseFlashcardFragment extends Fragment {
         }
     }
 
-    @OnClick(R.id.card_image)
-    public void openImageInFullView() {
-        String imageUrl = isShowingTerm
-                ? flashcard.getTermImageUrl()
-                : flashcard.getDefinitionImageUrl();
-        Activity activity = getActivity();
-        if (!TextUtils.isEmpty(imageUrl) && activity != null) {
-            Intent intent = new Intent(activity, PictureFullViewActivity.class)
-                    .putExtra(Constants.IMAGE_URL_KEY, imageUrl)
-                    .putExtra(Constants.CAPTION_KEY, isShowingTerm
-                            ? flashcard.getTerm()
-                            : flashcard.getDefinition());
-            activity.startActivity(intent);
-            activity.overridePendingTransition(R.anim.fade_in, 0);
+    private final View.OnClickListener imageClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String imageUrl = isShowingTerm
+                    ? flashcard.getTermImageUrl()
+                    : flashcard.getDefinitionImageUrl();
+            Activity activity = getActivity();
+            if (!TextUtils.isEmpty(imageUrl) && activity != null) {
+                Intent intent = new Intent(activity, PictureFullViewActivity.class)
+                        .putExtra(Constants.IMAGE_URL_KEY, imageUrl)
+                        .putExtra(Constants.CAPTION_KEY, isShowingTerm
+                                ? flashcard.getTerm()
+                                : flashcard.getDefinition());
+                activity.startActivity(intent);
+                activity.overridePendingTransition(R.anim.fade_in, 0);
+            }
         }
+    };
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        loadFlashcardIntoView();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        fullTextDialog.cleanUp();
         settingsManager.removeListener(defaultSideListener);
         unbinder.unbind();
     }
--- app/src/main/java/com/randomappsinc/simpleflashcards/utils/ViewUtils.java
+++ app/src/main/java/com/randomappsinc/simpleflashcards/utils/ViewUtils.java
@@ -1,8 +1,5 @@
 package com.randomappsinc.simpleflashcards.utils;
 
-import android.text.Layout;
-import android.text.StaticLayout;
-import android.text.TextPaint;
 import android.view.View;
 import android.view.ViewTreeObserver;
 
@@ -17,24 +14,5 @@ public class ViewUtils {
                 return false;
             }
         });
-    }
-
-    public static int measureHeightOfText(
-            final CharSequence text,
-            final int textSize,
-            final int textViewWidth) {
-        TextPaint myTextPaint = new TextPaint();
-        myTextPaint.setAntiAlias(true);
-        myTextPaint.setTextSize(textSize);
-        Layout.Alignment alignment = Layout.Alignment.ALIGN_NORMAL;
-        StaticLayout myStaticLayout = new StaticLayout(
-                text,
-                myTextPaint,
-                textViewWidth,
-                alignment,
-                1f,
-                0,
-                false);
-        return myStaticLayout.getHeight();
     }
 }
